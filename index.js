f247f23220765e4672152d50912c29b6da3012cf
idlej <muxiatianzhong@gmail.com>
2019-05-15 11:25:18 +0800

    init

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const https = require('https');
+const axios = require('axios');
+
+class ZabbixApi {
+  constructor(usr, pwd, host, port, useSSL) {
+    this.usr = usr;
+    this.pwd = pwd;
+    if (useSSL) {
+      port = port || 443;
+      this.axios = axios.create({
+        baseURL: `https://${host}:${port}/api_jsonrpc.php`,
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false
+        })
+      });
+    } else {
+      port = port || 80;
+      this.axios = axios.create({
+        baseURL: `http://${host}:${port}/api_jsonrpc.php`
+      });
+    }
+    this.auth = '';
+    this.id = 1;
+  }
+
+  async request(method, params) {
+    params = params || {};
+    let data = {
+      jsonrpc: '2.0',
+      method,
+      params,
+      id: this.id
+    }
+    this.id += 1;
+    if (this.auth && method != 'apiinfo.version' && method != 'user.login') {
+      data.auth = this.auth;
+    }
+    let response = await this.axios.post('', data);
+    if (response.status != 200) {
+      throw `Received response with error: ${response.status}`;
+    }
+    let responseData = response.data;
+    if ('error' in responseData) {
+      let error = responseData.error;
+      if (['Session terminated, re-login, please.', 'Not authorised.'].includes(error.data)) {
+        await this.login();
+        return await this.request(method, params);
+      }
+      throw `Error ${error.code}: ${error.message}, ${error.data}`;
+    }
+    return responseData.result;
+  }
+
+  async login() {
+    let params = {
+      user: this.usr,
+      password: this.pwd
+    };
+    this.auth = await this.request('user.login', params);
+  }
+}
+
+module.exports = ZabbixApi;
